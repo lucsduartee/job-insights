b1369bcae57680399138a29b24eb7f25e62e02ed
Lucas Duarte <lucs.rkt@gmail.com>
2022-08-09 19:06:10 -0300

    test: implement test to read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for job in jobs:
+        assert "title" in job and "titulo" not in job
+        assert "salary" in job and "salario" not in job
+        assert "type" in job and "tipo" not in job
